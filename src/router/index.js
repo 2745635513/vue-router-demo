acf85406b69c226cc4ca0c86c4baa06875f0ed09
FRACE <frace@inmoods.com>
2021-06-02 22:29:42 +0800

    命名视图的同级组件展示

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,59 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/settings",
+    // You could also have named views at tho top
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../components/named-views/UserSettings.vue"
+      ),
+    children: [
+      {
+        path: "emails",
+        component: () =>
+          import(
+            /* webpackChunkName: "about" */ "../components/named-views/UserEmailsSubscriptions.vue"
+          )
+      },
+      {
+        path: "profile",
+        components: {
+          default: () =>
+            import(
+              /* webpackChunkName: "about" */ "../components/named-views/UserProfile.vue"
+            ),
+          helper: () =>
+            import(
+              /* webpackChunkName: "about" */ "../components/named-views/UserProfilePreview.vue"
+            )
+        }
+      }
+    ]
   }
-]
+];
 
 const router = new VueRouter({
+  mode: "history",
   routes
-})
+});
 
-export default router
+export default router;
