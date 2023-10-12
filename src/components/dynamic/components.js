82698e4d66373200b90455789cdc70023e47db5f
FRACE <frace@inmoods.com>
2023-10-12 11:59:41 +0800

    动态路由懒加载

---
+++ src/components/dynamic/components.js
@@ -0,0 +1,27 @@
+export default {
+  '113D5335AAF7E5E': {
+    name: 'page1',
+    path: '/page1',
+    component: () => import(/* webpackChunkName: "dynamic-1" */ './1.vue')
+  },
+  '3BC4665FCB73F85': {
+    name: 'page2',
+    path: '/page2',
+    component: () => import(/* webpackChunkName: "dynamic-2" */ './2.vue')
+  },
+  '4F3D26AAEBC78F8': {
+    name: 'page3',
+    path: '/page3',
+    component: () => import(/* webpackChunkName: "dynamic-3" */ './3.vue')
+  },
+  '213799DD26F41CC': {
+    name: 'page4',
+    path: '/page4',
+    component: () => import(/* webpackChunkName: "dynamic-4" */ './4.vue')
+  },
+  '5434A50E6A85897': {
+    name: 'page5',
+    path: '/page5',
+    component: () => import(/* webpackChunkName: "dynamic-5" */ './5.vue')
+  }
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <router-link to="/page1">page1</router-link>
   </div>
 </template>
+
+<script>
+import dynamicRoutes from '../components/dynamic/components.js';
+
+export default {
+
+  mounted () {
+    const dynamicRouteKeys = ['113D5335AAF7E5E', '3BC4665FCB73F85', '4F3D26AAEBC78F8', '213799DD26F41CC', '5434A50E6A85897']
+    dynamicRouteKeys.forEach(key => {
+      this.$router.addRoute(dynamicRoutes[key]);
+      // this.$router.options.routes.push(dynamicRoutes[key])
+    })
+  }
+}
+</script>
